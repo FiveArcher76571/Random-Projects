4f5913770bf2f6fceb735c67cdd70ac5b4a48f3e
FiveArcher76571 <vemuri.saicharan@gmail.com>
2021-04-11 14:45:41 -0400

    Create keyboard-python folder and corresponding README.md

---
+++ keyboard-python/README.md
@@ -0,0 +1,4 @@
+# Keyboard Control Test
+
+Testing how the keyboard module works
+Changes "fuck" to "NONO WORD"
---
+++ keyboard-python/keyboard-python.py
@@ -0,0 +1,10 @@
+#Saicharan Vemuri (FiveArcher76571)
+#Started Apr. 7, 2021
+#Keyboard Control Test
+#Requires keyboard module (needs to be installed, I think)
+
+import keyboard
+
+keyboard.add_abbreviation("fuck", "NONO WORD")
+
+input("Press Enter to end...")
