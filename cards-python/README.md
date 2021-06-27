82e85a48212b22eb58620cd03ba602c8c96dde84
FiveArcher76571 <vemuri.saicharan@gmail.com>
2021-06-26 22:57:41 -0400

    Create cards-python

---
+++ cards-python/README.md
@@ -0,0 +1,7 @@
+# Cards
+
+Creating a deck of cards using Python
+
+Made using the Raspberry Pi Guide
+
+https://projects.raspberrypi.org/en/projects/deck-of-cards/
---
+++ cards-python/cards-python.py
@@ -0,0 +1,14 @@
+#Saicharan Vemuri (FiveArcher76571)
+#Cards
+#Made using the tutorial at https://projects.raspberrypi.org/en/projects/deck-of-cards/
+#Started June 26th, 2021
+
+import random
+
+class Card:
+    def __init__(self, suit, num):
+        self.suit = suit
+        self.num = num
+
+    def __repr__(self):
+        return self.num 
