ed15e8de59fc15b2c297c17627b625105d1da750
FiveArcher76571 <vemuri.saicharan@gmail.com>
2021-07-25 21:16:05 -0400

    Add finger management logic. Rough edit.

--- chopsticks-python/chopsticks.py
+++ chopsticks-python/chopsticks.py
@@ -5,35 +5,40 @@
 import random
 
 def p1turn():
-    p1prep = str(input("Which hand do you want to use? (l for left and r for right):\n"))
+
+    p1prep = str(input("Which hand do you want to use? Or do you want to manage your own hands? (l for left, r for right, m for manage):\n"))
     p1prep = p1prep.lower()
-    p1atk = str(input("Which hand do you want to tap? (l for left and r for right):\n"))
-    p1atk = p1atk.lower()
 
-    if (p1prep == "l"):
-        if (p1atk == "l"):
-            p2left += p1left
-        elif (p1atk == "r"):
-            p2right += p1left
+    if (p1prep == "m"):
+        #"n + (x - n)"; Ask for n and fill in blanks
+        p1fingers = p1left + p1right
+        varN = int(input("How many fingers do you want on your left hand?\n"))
+        p1right = varN
+        p1left = p1fingers - varN
+    
+    elif (p1prep == "l" || p1prep == "r"):
+        p1atk = str(input("Which hand do you want to tap? (l for left, r for right, m for manage):\n"))
+        p1atk = p1atk.lower()
+
+        if (p1prep == "l"):
+            if (p1atk == "l"):
+                p2left += p1left
+            elif (p1atk == "r"):
+                p2right += p1left
+            else:
+                print("Error bruh")
+        elif (p1prep == "r"):
+            if (p1atk == "l"):
+                p2left += p1right
+            elif (p1atk == "r"):
+                p2right += p1right
+            else:
+                print("Error bruh")
         else:
             print("Error bruh")
-    elif (p1prep == "r"):
-        if (p1atk == "l"):
-            p2left += p1right
-        elif (p1atk == "r"):
-            p2right += p1right
-        else:
-            print("Error bruh")
 
-def game():
 
-    p1left = 1
-    p1right = 1
-    p2left = 1
-    p2right = 1
-
-    
-    
+def p2turn():
     p2prep = str(input("Which hand do you want to use? (l for left and r for right):\n"))
     p2prep = p2prep.lower()
     p2atk = str(input("Which hand do you want to tap? (l for left and r for right):\n"))
@@ -53,5 +58,16 @@ def game():
             p1right += p2right
         else:
             print("Error bruh")
+
+
+def game():
+
+    p1left = 1
+    p1right = 1
+    p2left = 1
+    p2right = 1
+
+    p1turn()
+    p2turn()
 
     
