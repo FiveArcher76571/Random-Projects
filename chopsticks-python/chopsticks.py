dcc6dd2a29afb1e75f56945d7a95394604f21110
FiveArcher76571 <vemuri.saicharan@gmail.com>
2021-08-28 16:26:16 -0400

    Added second player (broken commit)

--- chopsticks-python/chopsticks.py
+++ chopsticks-python/chopsticks.py
@@ -4,7 +4,21 @@
 
 import random
 
+p1left = 1
+p1right = 1
+p1fingers = 0
+p2left = 1
+p2right = 1
+p2fingers = 0
+
 def p1turn():
+
+    p1left = 1
+    p1right = 1
+    p1fingers = 0
+    p2left = 1
+    p2right = 1
+    p2fingers = 0
 
     p1prep = str(input("Which hand do you want to use? Or do you want to manage your own hands? (l for left, r for right, m for manage):\n"))
     p1prep = p1prep.lower()
@@ -16,7 +30,7 @@ def p1turn():
         p1right = varN
         p1left = p1fingers - varN
     
-    elif (p1prep == "l" || p1prep == "r"):
+    elif (p1prep == "l" or p1prep == "r"):
         p1atk = str(input("Which hand do you want to tap? (l for left, r for right, m for manage):\n"))
         p1atk = p1atk.lower()
 
@@ -36,7 +50,6 @@ def p1turn():
                 print("Error bruh")
         else:
             print("Error bruh")
-
 
 def p2turn():
     p2prep = str(input("Which hand do you want to use? (l for left and r for right):\n"))
@@ -59,15 +72,4 @@ def p2turn():
         else:
             print("Error bruh")
 
-
-def game():
-
-    p1left = 1
-    p1right = 1
-    p2left = 1
-    p2right = 1
-
-    p1turn()
-    p2turn()
-
-    
+p1turn()
